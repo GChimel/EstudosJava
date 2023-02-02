146786da0d1fec84ee34102f1f423ca8bb4adf1d
GChimel <gustdeveloper@gmail.com>
2023-02-02 19:41:01 -0300

    Exercícios beecrowd

---
+++ Exercicios/src/beecrowd.java
@@ -0,0 +1,20 @@
+// Esse arquivo vai ser utilizado para realizar os exercicios do beecrowd.
+
+//Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+//Entrada
+//O arquivo de entrada contém 2 valores inteiros.
+//Saída
+//Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B. Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+public class beecrowd {
+    public static void main(String[] args) {
+
+        int A = 30;
+        int B = 10;
+        int soma;
+
+        soma = A + B;
+
+        System.out.println("SOMA = " + soma);
+
+    }
+}
