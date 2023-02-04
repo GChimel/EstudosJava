720834959686ff24363e8f587a65f465fd810a1e
GChimel <gustdeveloper@gmail.com>
2023-02-03 21:15:12 -0300

    Exercício MÉDIA 2 do beecrowd

--- Exercicios/src/beecrowd.java
+++ Exercicios/src/beecrowd.java
@@ -1,27 +1,28 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-//Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+//Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
 //
 //        Entrada
-//        O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+//        O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
 //
 //        Saída
-//        Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. Utilize variáveis de dupla precisão (double) e como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá Presentation Error.
+//        Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade. Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá Presentation Error.
 public class beecrowd {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double A,B,med;
+        double A, B, C, med;
 
         A = sc.nextDouble();
         B = sc.nextDouble();
+        C = sc.nextDouble();
 
-        med  = ((A * 3.5) + (B * 7.5)) / 11;
+        med = ((A * 2) + (B * 3) + (C * 5)) / 10;
 
-        System.out.printf("MEDIA = %.5f\n", med);
+        System.out.printf("Media = %.1f\n", med);
 
     }
 }
