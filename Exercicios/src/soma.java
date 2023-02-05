571815edeab7aaa2e1686066860e26d68eb6fc80
GChimel <gustdeveloper@gmail.com>
2023-02-05 17:33:26 -0300

    Exercício sobre funções matemáticas

---
+++ Exercicios/src/soma.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class soma {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A,B,MED;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        MED = A + B;
+        System.out.println("SOMA = " + MED);
+    }
+}
