e16720f773c1f08ef641b8a3ef3029002f27ea1b
GChimel <gustdeveloper@gmail.com>
2023-03-01 19:19:14 -0300

    Resolução do bug e exercicios

---
+++ Exercicios/src/CursoUdemy/ExercicioFor5.java
@@ -0,0 +1,20 @@
+package CursoUdemy;
+
+import java.util.Scanner;
+
+public class ExercicioFor5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe o número para saber seu fatorial: ");
+        int n = sc.nextInt();
+
+
+        for () {
+
+        }
+
+
+        sc.close();
+    }
+}
