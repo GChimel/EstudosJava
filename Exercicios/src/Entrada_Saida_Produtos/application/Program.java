27484ede494214d1a09fec4e226a96deba7946b2
GChimel <gustdeveloper@gmail.com>
2023-03-09 13:37:49 -0300

    preparatorio entradaESaida

--- Exercicios/src/Entrada_Saida_Produtos/application/Program.java
+++ Exercicios/src/Entrada_Saida_Produtos/application/Program.java
@@ -1,7 +1,29 @@
 package Entrada_Saida_Produtos.application;
 
+import Entrada_Saida_Produtos.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+
+        System.out.print("Enter product data: ");
+
+        System.out.println("Name: ");
+        product.name = sc.nextLine();
+        System.out.println("Price: ");
+        product.price = sc.nextDouble();
+        System.out.println("Quantity: ");
+        product.quantity = sc.nextInt();
+
+        System.out.printf(product.name + ", " + product.price + ", " + product.quantity);
+
+        sc.close();
     }
 }
