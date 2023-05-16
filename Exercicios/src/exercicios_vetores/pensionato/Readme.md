91405e98d1b3cac95e1ca7a63f8804b2718c436f
GChimel <gustdeveloper@gmail.com>
2023-05-16 20:12:47 -0300

    exercicios vetores (pensionato v1)

---
+++ Exercicios/src/exercicios_vetores/pensionato/Readme.md
@@ -0,0 +1,11 @@
+# Objetivo do programa
+
+``A dona de um pensionato possui dez quartos para alugar para estudantes, sendo esses quartos identificados
+pelos números de 0 a 9.`` 
+
+``Fazer um programa que inicia com todos os dez quartos vazios, e depois dleia uma quantidade N representando
+o número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N
+estudantes. Para cada registro de aluguel, informar o nome e email do estudantem ben como qual dos quartos ele escolheu
+(0 a 9), Suponha que seja escolhido um quarto vago. Ao fginal, seu programa de ve imprimir um relatório de todas as
+ocupações do pensionato, por ordem de quarto, conforme exemplo.``
+
---
+++ Exercicios/src/exercicios_vetores/pensionato/application/Program.java
@@ -0,0 +1,40 @@
+package exercicios_vetores.pensionato.application;
+
+import exercicios_vetores.pensionato.entities.Rooms;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many rooms will be rented? ");
+        int n = input.nextInt();
+
+        Rooms[] rent = new Rooms[10];
+
+        int rentNumber = 1;
+        for(int i = 0; i < n ; i++) {
+            System.out.print("Rent #" +rentNumber++ + ":\n");
+            input.nextLine();
+            System.out.print("Name: ");
+            String name = input.nextLine();
+            System.out.print("Email: ");
+            String email = input.nextLine();
+            System.out.print("Room: ");
+            int room = input.nextInt();
+            rent[i] = new Rooms(name, email, room);
+        }
+
+        for(int i = 0; i < n; i++) {
+            if (rent[i] != null) {
+                System.out.println(rent[i].getRoom() + ": " + rent[i].getName() + ", " + rent[i].getEmail());
+            }
+        }
+
+        input.close();
+    }
+}
