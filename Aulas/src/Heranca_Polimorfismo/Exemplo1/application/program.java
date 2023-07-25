d49971297106e472cd8c4b4fbed7f8c0ce42c04a
GChimel <gustdeveloper@gmail.com>
2023-07-24 21:09:44 -0300

    aula 1 sobre heranca

---
+++ Aulas/src/Heranca_Polimorfismo/Exemplo1/application/program.java
@@ -0,0 +1,11 @@
+package Heranca_Polimorfismo.Exemplo1.application;
+
+import Heranca_Polimorfismo.Exemplo1.entities.businessAccount;
+
+public class program {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ Aulas/src/Heranca_Polimorfismo/Exemplo1/entities/businessAccount.java
@@ -0,0 +1,28 @@
+package Heranca_Polimorfismo.Exemplo1.entities;
+
+public class businessAccount extends account{
+
+    private Double loanLimit;
+
+    public businessAccount() {
+        super();
+    }
+    public businessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount) {
+        if(amount <= loanLimit) {
+            deposit(amount);
+        }
+    }
+}
