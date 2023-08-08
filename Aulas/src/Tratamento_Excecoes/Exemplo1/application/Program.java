33a65d34f7a1171d4d1af1aab2cb4a32780c8072
GChimel <gustdeveloper@gmail.com>
2023-08-07 21:54:10 -0300

    try-catch com .printStackTracer();

--- Aulas/src/Tratamento_Excecoes/Exemplo1/application/Program.java
+++ Aulas/src/Tratamento_Excecoes/Exemplo1/application/Program.java
@@ -1,11 +1,27 @@
 package Tratamento_Excecoes.Exemplo1.application;
 
+import java.sql.SQLOutput;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        method1();
+        System.out.println("End of program!");
+
+        sc.close();
+    }
+
+    public static void method1() {
+        System.out.println("***METOD1 START***");
+        method2();
+        System.out.println("***METOD1 END***");
+    }
+    public static void method2() {
+        System.out.println("***METHOD2 START***");
         Scanner sc = new Scanner(System.in);
 
         try {
@@ -17,13 +33,14 @@ public class Program {
         //Tratamento de posição inválida.
         catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position!");
+            //Utilizado para exibir toda a chamada de métodos na seção.
+            e.printStackTrace();
         }
         //Tratamento de valor inválido.
         catch (InputMismatchException e) {
             System.out.println("Input error!");
         }
-        System.out.println("End of program");
-
+        System.out.println("***METOD2 END***");
         sc.close();
     }
 }
